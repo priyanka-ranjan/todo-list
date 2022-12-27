ac94ecfa9e88a84979401441a3959ec50f7c1590
Priyanka Ranjan <priyankaranjan@gmail.com>
2022-12-27 11:41:05 +0400

    Added @Valid validation for the description field

--- src/main/kotlin/com/springboot/webapp/todo/todo/Todo.kt
+++ src/main/kotlin/com/springboot/webapp/todo/todo/Todo.kt
@@ -1,11 +1,14 @@
 package com.springboot.webapp.todo.todo
 
+import jakarta.validation.constraints.Size
 import java.time.LocalDate
 
 data class Todo(
     val id: Int,
-    val username: String,
+    val username: String?,
+
+    @field:Size(min = 10, message = "Enter at least 10 characters")
     val description: String,
-    val targetDate: LocalDate,
-    val done: Boolean
+    val targetDate: LocalDate?,
+    val done: Boolean = false
 )
--- src/main/kotlin/com/springboot/webapp/todo/todo/TodoController.kt
+++ src/main/kotlin/com/springboot/webapp/todo/todo/TodoController.kt
@@ -1,10 +1,11 @@
 package com.springboot.webapp.todo.todo
 
+import jakarta.validation.Valid
 import org.springframework.stereotype.Controller
 import org.springframework.ui.ModelMap
+import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.SessionAttributes
 import java.time.LocalDate
 
@@ -24,19 +25,44 @@ class TodoController (
 
     //GET, POST
     @RequestMapping(value = ["add-todo"], method = [RequestMethod.GET])
-    fun showNewTodoPage(): String? {
+    fun showNewTodoPage(model: ModelMap): String? {
+        val todo = Todo(
+            0,
+            model["name"] as String? ?: "",
+            "Default Description",
+            LocalDate.now().plusYears(1),
+            false)
+        model.put("todo", todo)
         return "add-todo"
     }
 
     @RequestMapping(value = ["add-todo"], method = [RequestMethod.POST])
-    fun addNewTodo(@RequestParam description: String?, model: ModelMap): String? {
-        val username = model["name"] as String?
+    fun addNewTodo(model: ModelMap,  @Valid todo: Todo, result: BindingResult): String? {
+
+        if (result.hasErrors()) {
+            return "redirect:add-todo"
+        }
+
+        val username = model["name"] as String? ?: ""
         todoService.addTodo(
             username,
-            description,
+            todo.description,
             LocalDate.now().plusYears(1),
             false
         )
         return "redirect:todo"
     }
+
+//    @RequestMapping(value = ["add-todo"], method = [RequestMethod.POST])
+//    fun addNewTodo(model: ModelMap, todo: @Valid Todo?, result: BindingResult): String? {
+//        if (result.hasErrors()) {
+//            return "todo"
+//        }
+//        val username = model["name"] as String?
+//        todoService.addTodo(
+//            username, todo!!.description,
+//            LocalDate.now().plusYears(1), false
+//        )
+//        return "redirect:list-todos"
+//    }
 }
