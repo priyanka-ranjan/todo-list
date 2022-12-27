659458f878b02fea5796c5804c5d98a371024f18
Priyanka Ranjan <priyankaranjan@gmail.com>
2022-12-27 17:08:18 +0400

    Removed commented out code

--- src/main/kotlin/com/springboot/webapp/todo/todo/TodoService.kt
+++ src/main/kotlin/com/springboot/webapp/todo/todo/TodoService.kt
@@ -10,32 +10,20 @@ class TodoService(
     val todoRepository: TodoRepository
 ) {
 
-//    companion object {
-//        var todosCount: Int = 0
-//
-//        private val todo1 = Todo(++todosCount,"username", "Learn AWS", LocalDate.now().plusYears(1), false)
-//        private val todo2 = Todo(++todosCount,"username", "Learn Azure", LocalDate.now().plusYears(2), false)
-//        private val todo3 = Todo(++todosCount,"username", "Learn FullStack", LocalDate.now().plusYears(3), false)
-//        val list = arrayOf(todo1, todo2, todo3).toMutableList()
-//    }
-
     fun findByUsername(username:String): Array<Todo> {
         return todoRepository.findByUsername(username).toTypedArray()
     }
 
     fun addTodo(todo:Todo) {
         todoRepository.save(todo)
-//        list.add(todo)
     }
 
     fun deleteById(id: Int) {
         todoRepository.deleteById(id)
-//        list.removeIf { it.id == id }
     }
 
     fun findById(id: Int): Todo {
         return todoRepository.findById(id).orElseThrow()
-//        return  list.first { it.id == id }
     }
 
     fun updateTodo(newtodo: Todo) {
